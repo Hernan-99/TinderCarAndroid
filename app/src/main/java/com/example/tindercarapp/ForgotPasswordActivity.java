40771a44c341888ebba9953feb3eea2d969aca09
Damian Rodriguez <damian.hr.rodriguez@gmail.com>
2023-09-06 21:56:56 -0300

    agregue la vista de ForgotPassword y la clase tambien

---
+++ app/src/main/java/com/example/tindercarapp/ForgotPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.example.tindercarapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.forgot_password);
+    }
+
+}
