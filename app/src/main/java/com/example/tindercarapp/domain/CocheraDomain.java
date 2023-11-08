5b17c76b0bad46ee53f69d4b87c7b672dcf16b71
Hernan <hernan24744@gmail.com>
2023-11-08 10:55:53 -0300

    clases domain para cocheras y vehiculos

--- app/src/main/java/com/example/tindercarapp/domain/CocheraDomain.java
+++ app/src/main/java/com/example/tindercarapp/domain/CocheraDomain.java
@@ -1,2 +1,39 @@
-package com.example.tindercarapp.domain;public class CocheraDomain {
+package com.example.tindercarapp.domain;
+
+public class CocheraDomain {
+
+    private String imgCochera;
+    private String tituloCochera;
+    private String descripcionCochera;
+
+    public CocheraDomain(String imgCochera, String tituloCochera, String descripcionCochera) {
+        this.imgCochera = imgCochera;
+        this.tituloCochera = tituloCochera;
+        this.descripcionCochera = descripcionCochera;
+    }
+
+
+    public String getImgCochera() {
+        return imgCochera;
+    }
+
+    public void setImgCochera(String imgCochera) {
+        this.imgCochera = imgCochera;
+    }
+
+    public String getTituloCochera() {
+        return tituloCochera;
+    }
+
+    public void setTituloCochera(String tituloCochera) {
+        this.tituloCochera = tituloCochera;
+    }
+
+    public String getDescripcionCochera() {
+        return descripcionCochera;
+    }
+
+    public void setDescripcionCochera(String descripcionCochera) {
+        this.descripcionCochera = descripcionCochera;
+    }
 }
--- app/src/main/java/com/example/tindercarapp/domain/VehiculoDomain.java
+++ app/src/main/java/com/example/tindercarapp/domain/VehiculoDomain.java
@@ -1,2 +1,27 @@
-package com.example.tindercarapp.domain;public class VehiculoDomain {
+package com.example.tindercarapp.domain;
+
+public class VehiculoDomain {
+    private String tipoVehiculo;
+    private String iconoVehiculo;
+
+    public VehiculoDomain(String tipoVehiculo, String iconoVehiculo) {
+        this.tipoVehiculo = tipoVehiculo;
+        this.iconoVehiculo = iconoVehiculo;
+    }
+
+    public String getTipoVehiculo() {
+        return tipoVehiculo;
+    }
+
+    public void setTipoVehiculo(String tipoVehiculo) {
+        this.tipoVehiculo = tipoVehiculo;
+    }
+
+    public String getIconoVehiculo() {
+        return iconoVehiculo;
+    }
+
+    public void setIconoVehiculo(String iconoVehiculo) {
+        this.iconoVehiculo = iconoVehiculo;
+    }
 }
